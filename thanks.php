bb635d172e683f2e6281a699818610caeb356d0c
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-07-22 11:38:53 -0400

    Update thanks.php

---
+++ thanks.php
@@ -0,0 +1,53 @@
+<?php include "header.php";?>
+<a href="/" style="display:block; margin: 20px;"><lord-icon
+    src="https://cdn.lordicon.com/iifryyua.json"
+    trigger="hover"
+    colors="primary:#f4791f"
+    state="hover-1"
+    style="width:32px;height:32px; -webkit-transform: scaleX(-1); transform: scaleX(-1);">
+</lord-icon> Back</a>
+<section class="ftco-appointment" id="contactUs">
+<div class="overlay"></div>
+<div class="container-wrap">
+<div class="row no-gutters d-md-flex align-items-center">
+<div class="col-md-6 appointment ftco-animate">
+<h2 class="mb-3">Reach Us</h2>
+<div class="block-23 mb-3">
+<ul>
+<li>
+<div class="icon"><lord-icon
+    src="https://cdn.lordicon.com/hcndxtmn.json"
+    trigger="loop"
+    colors="primary:#f4791f"
+    style="width:32px;height:32px">
+</lord-icon></div>
+<div class="text">
+<h4><a href="tel:+1 (519) 981 8039">+1 (519) 981 8039</a></h4>
+</div>
+</li>
+<li>
+<div class="icon"><lord-icon
+    src="https://cdn.lordicon.com/iicmtpsq.json"
+    trigger="loop"
+    colors="primary:#f4791f"
+    style="width:32px;height:32px">
+</lord-icon></div>
+<div class="text">
+<h4><a><span class="__cf_email__">abc@abc.com</span></a></h4>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="col-md-6 appointment ftco-animate">
+<h3 class="mb-3">Contact Us</h3>
+<div class="media-body">
+<p>Your order has been recieved. Thank you for your interest in <span class="subheading">My Lunchbox</span>.</p>
+</div>
+<?php include "form.php";?>
+</div>
+</div>
+</div>
+</section>
+
+<?php include "footer.php";?>
