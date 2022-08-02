af75d33dd40cf8a937e5e6fe97714ecbb9da888d
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-08-02 15:02:52 -0400

    Content Update

--- form.php
+++ form.php
@@ -14,8 +14,23 @@
         <input type="text" name="order" required class="form-control contact_field" placeholder="Order Description">
     </div>
     </div>
+    <div class="d-me-flex">
     <div class="form-group">
-        <textarea name="address" id="" cols="30" required rows="3" class="form-control contact_field" placeholder="Address"></textarea>
+        <label for="quantity">Quantity:</label>
+        <input type="number" name="Quantity" min="1" value="1">
+    </div>
+    </div>
+    <div class="d-me-flex">
+    <div class="form-group">
+        <p>Do you want your Lunchbox delivered?<span>Delivery: $2.49 per address</span></p>
+        <input type="radio" name="Delivery" required class="form-control contact_field">
+        <label for="Yes">Yes</label>
+        <input type="radio" name="Delivery" class="form-control contact_field">
+        <label for="No">No</label>
+    </div>
+    </div>
+    <div class="form-group">
+        <textarea name="address" id="" cols="30" rows="3" class="form-control contact_field" placeholder="Address"></textarea>
     </div>
     <div class="form-group">
         <button type="submit" value="Send" class="btn btn-primary submit_button py-3 px-4">Send</button>
