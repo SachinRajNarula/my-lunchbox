c11acb0a1729ea380509864825d62f523f934f24
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-08-09 16:10:52 -0400

    Form Update

--- thanks.php
+++ thanks.php
@@ -11,7 +11,12 @@ include "header.php";
 </lord-icon> Back</a>
 
 <div class="media-body" style="padding: 100px 30px 100px 30px; text-align: center;">
+<<<<<<< Updated upstream
 <p>Your message has been recieved. Thank you for your interest in <span class="subheading">My Lunchbox</span>.</p>
+=======
+<p>Your message has been recieved. Thank you for your interest in <a href="/"><span class="subheading">My Lunchbox</span></a>.</p>
+<p>If you have placed a new order then you might recieve a quick call or text for confirmation.</p> 
+>>>>>>> Stashed changes
 </div>
 
 
