8d5dc61cbcb301de7be94c0fe9103a61dabdb7cf
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-08-10 22:12:34 -0400

    GA Tags

--- header.php
+++ header.php
@@ -29,7 +29,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WQ3FCTX');</script>
 <!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-237129164-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-237129164-1');
+</script>
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
