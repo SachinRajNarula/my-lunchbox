e04c6553948eaa0665a3e718d14afc462d9cd2dd
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-08-30 09:55:44 -0400

    Calendar

--- menu.php
+++ menu.php
@@ -13,4 +13,5 @@ include "header.php";
 <div data-tockify-component="calendar" data-tockify-calendar="lunchboxmenu"></div>
 <script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
 </div>
+<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23f47910&ctz=America%2FToronto&showTitle=1&showNav=1&showDate=1&showPrint=0&showTabs=1&showTz=1&src=NXQ0NTVjMW00ZHU4Z2NrczhiMjllb3NzaGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23E67C73" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 <?php include "footer.php";?>
